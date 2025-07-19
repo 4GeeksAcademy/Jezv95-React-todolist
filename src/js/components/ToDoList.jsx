c0ea26ffd3abd64c35bd375bb73834a4b672c576
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-07-19 01:33:10 +0000

    creado componente y array para lista

---
+++ src/js/components/ToDoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ToDoList = () => {
+
+    let Tasks = ['Make the Bed','Wash My Hands','Eat','Walk the dog']
+
+   
+
+    return (
+        <>
+        <div>
+            <h1>To Do List</h1>
+            
+           <ul>{TaskList}</ul>
+            </div>
+       
+       </>
+    );
+};
+
+export default ToDoList;
